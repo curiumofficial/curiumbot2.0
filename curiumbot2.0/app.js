1984e69867a08618d0d8fa5824d0a725d1908f0a
mrmetech <31669092+mrmetech@users.noreply.github.com>
2018-04-29 19:59:42 -0600

    added help msg

--- curiumbot2.0/app.js
+++ curiumbot2.0/app.js
@@ -1,3 +1,52 @@
-'use strict';
+const Discord = require('discord.js');
+const client = new Discord.Client();
 
-console.log('Hello world');
+const config = require("./config.json");
+const prefix = "!"; 
+
+
+
+client.on('ready', () => {
+    console.log(`Logged in as ${client.user.tag}!`);
+});
+
+
+
+client.on('message', !help => {
+    message.channel.send({
+        embed: {
+            color: 3447003,
+            author: {
+                name: client.user.username,
+                icon_url: client.user.avatarURL
+            },
+            title: "Bot Commands",
+            description: "These are commands for the bot Beep Boop",
+            fields: [{
+                name: "!cru",
+                value: "These command will give you curiums price"
+            },
+            {
+                name: "!command",
+                value: "You can put [masked links](http://google.com) inside of rich embeds."
+            },
+            {
+                name: "Markdown",
+                value: "You can put all the *usual* **__Markdown__** inside of them."
+            }
+            ],
+            timestamp: new Date(),
+            footer: {
+                icon_url: client.user.avatarURL,
+                text: "© crubot"
+            }
+        }
+    });
+});
+
+
+
+
+
+
+client.login('token');
