8d4c4a00eb88bc198f899c0afe04f6964a989837
mrmetech <31669092+mrmetech@users.noreply.github.com>
2018-04-29 22:16:56 -0600

    added price for btc

--- curiumbot2.0/app.js
+++ curiumbot2.0/app.js
@@ -41,6 +41,33 @@ client.on('message', !help => {
     });
 });
 
+client.on('message', !cru => {
+    var price = 'https://api.coinmarketcap.com/v1/ticker/bitcoin/';
+    var pprice = getJSON(price);
+    var kprice = JSON.parse(pprice);
+    kprice.price_usd = new pri(kprice.price_usd)
+    message.channel.send({
+        embed: {
+            color: 3447003,
+            author: {
+                name: client.user.username,
+                icon_url: client.user.avatarURL
+            },
+            title: "CRU price",
+            description: "This is the prices for curium",
+            fields: [{
+                value: "This will give you a list of commands for the bot!" + kprice.price_usd
+            }
+
+            ],
+            timestamp: new Date(),
+            footer: {
+                icon_url: client.user.avatarURL,
+                text: "© crubot"
+            }
+        }
+    });
+});
 
 
 
